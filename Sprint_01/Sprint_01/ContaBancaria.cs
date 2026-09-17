4fef7f19a4ecc47347024d05c859c503df43dc42
Hugo Colognese <hugocolognese@gmail.com>
2026-09-17 12:06:05 -0300

    Add files via upload

---
+++ Sprint_01/Sprint_01/ContaBancaria.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sprint_01
+{
+    class ContaBancaria
+    {
+        public int NumeroConta { get; set; }
+        public string Titular { get; set; }
+        public decimal Saldo { get; set; }
+
+        public void ExibirDados()
+        {
+            Console.WriteLine($"titular da conta: {Titular}");
+            Console.WriteLine($"numero da conta: {NumeroConta}");
+            Console.WriteLine($"saldo: {Saldo}");
+        }
+    }
+}
---
+++ Sprint_01/Sprint_01/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Sprint_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaBancaria informacoesConta = new ContaBancaria();
+
+            informacoesConta.Titular = "Hugo";
+            informacoesConta.NumeroConta = 123456;
+            informacoesConta.Saldo = 15.50m;
+            informacoesConta.ExibirDados();
+        }
+    }
+}
